785267b311d0e8edcc2dd5e6f68d4f7a298f6de0
Nick Liccini <Nick.liccini@gmail.com>
2016-08-09 18:18:13 -0400

    Keypress to control the robot

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 const _ = require('lodash');
 const async = require('async');
+const keypress = require('keypress');
 
-const motions = [
-    'f',
-    'r',
-    'b',
-    'l'
-];
+const motions = {
+    'up': 'f',
+    'right': 'r',
+    'down': 'b',
+    'left': 'l',
+    'space': 's'
+};
+
+keypress(process.stdin);
 
 btSerial.on('found', (address, name) => {
     console.log(`I Found a Device: ${address} - ${name}`); 
@@ -16,42 +20,45 @@ btSerial.on('found', (address, name) => {
 	if (address !== '20-16-06-30-69-09') {
 	    return;
 	}
+
 	console.log(`For address: ${address}, trying this channel: ${channel}`);
 
         btSerial.connect(address, channel, () => {
+
             console.log(`connected to address: ${address} on channel: ${channel}`);
 
-	    const range = _.range(0, 10);
+	    process.stdin.on('keypress', (ch, key) => {
+	        console.log('got "keypress"', key);
 
-	    async.eachSeries(range, (i, callback) => {
-		const motion = motions[i % motions.length];
-                btSerial.write(new Buffer(motion, 'utf-8'), (err, bytesWritten) => {
+		const { name } = key;
+
+		if (name === 'c' && key.ctrl) {
+	            process.stdin.pause();
+
+		    btSerial.write(new Buffer('s', 'utf-8'), (err, bytesWritten) => {
+			btSerial.close();
+			console.log('Done!');
+		    });
+		}
+
+		const command = motions[name];
+
+		if (!command) {
+		    return;
+		}
+
+	        btSerial.write(new Buffer(command, 'utf-8'), (err, bytesWritten) => {
                     if (err) {
 		        console.log(err);
 		    } else {
 			console.log(`Successfully Wrote: ${bytesWritten} Bytes over Bluetooth`);
 		    }
-
-		    setTimeout(() => {
-		        callback(err);
-		    }, 2000);
-
                 });
-            }, (err) => {
-		if (err) {
-		    console.log(`Error Transmitting Data: ${err}`);
-		} else {
-		    console.log(`Transmission Complete`);
-		}
 
-		console.log('Sending Stop Command...');
-
-                btSerial.write(new Buffer('s', 'utf-8'), (err, bytesWritten) => {
-		    btSerial.close();
-		    console.log('Done!');
-		});
-
-            });
+	    });
+ 
+	    process.stdin.setRawMode(true);
+	    process.stdin.resume();
 
             btSerial.on('data', (buffer) => {
                 console.log(buffer.toString('utf-8'));
